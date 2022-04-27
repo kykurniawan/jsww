b5fa2cc8bcd051c0c1a8b4cee8a6979c1b8990c8
kykurniawan <contact.rizkykurniawan@gmail.com>
2022-04-28 00:37:06 +0800

    fix headers

--- pages/api/watchers/index.js
+++ pages/api/watchers/index.js
@@ -4,40 +4,39 @@ import { createWatcherSchema } from "../../../lib/validations/watcher";
 export default async function handler(req, res) {
   const { method, body, query } = req
 
-  switch (method) {
-    case "GET":
-      const watchers = await getUserWatchers(query.userId)
-      res.status(200).json({
-        status: 200,
-        message: "Ok",
+  if (method == 'GET') {
+    const watchers = await getUserWatchers(query.userId)
+    return res.status(200).json({
+      status: 200,
+      message: "Ok",
+      error: null,
+      data: {
+        watchers: watchers
+      }
+    })
+  }
+
+  if (method == 'POST') {
+    const { error, value } = createWatcherSchema.validate({ userId: body.userId, name: body.name })
+    if (error != undefined) {
+      return res.status(400).json({
+        status: 400,
+        message: "Validation error",
+        error: error,
+        data: null
+      })
+    } else {
+      const watcher = await createUserWatcher(value.userId, value.name)
+      return res.status(201).json({
+        status: 201,
+        message: "Watcher successfully created",
         error: null,
         data: {
-          watchers: watchers
+          watcher: watcher
         }
       })
-      break;
-    case "POST":
-      const { error, value } = createWatcherSchema.validate({ userId: body.userId, name: body.name })
-      if (error != undefined) {
-        res.status(400).json({
-          status: 400,
-          message: "Validation error",
-          error: error,
-          data: null
-        })
-      } else {
-        const watcher = await createUserWatcher(value.userId, value.name)
-        res.status(201).json({
-          status: 201,
-          message: "Watcher successfully created",
-          error: null,
-          data: {
-            watcher: watcher
-          }
-        })
-      }
-    default:
-      res.setHeader('Allow', ['GET', 'POST'])
-      res.status(405).end(`Method ${method} Not Allowed`)
+    }
   }
+
+  res.status(405).end(`Method ${method} Not Allowed`)
 }
